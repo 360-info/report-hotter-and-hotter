bb9cee8b5dc11679c621786539d1ed00d69f6c43
James Goldie <james.goldie@360info.org>
2022-02-21 09:46:49 +1100

    Convert to GH source

--- util.r
+++ util.r
@@ -1,5 +1,31 @@
+library(tidyverse)
 library(mosaic)
 library(cli)
+
+# pipe fn to import surveys and do light tidying
+import_survey <- . %>%
+  {
+    read_csv(., na = c("", "NA", "N/A", "N / A"),
+      col_types = cols(
+        "OptIn Date" = col_date(format = "%d/%m/%Y"),
+        HoursDay = col_integer(),
+        Rooms = col_integer(),
+        Windows = col_integer(),
+        "People in House" = col_number(),
+        Temperature = col_number(),
+        "Temperature (C)" = col_number(),
+        "Relative Humidity" = col_number(),
+        "Heat Index" = col_number(),
+        "Heat Index (C)" = col_number(),
+        .default = col_factor(NULL, include_na = FALSE)
+        # .default = col_character()
+    ))
+  } %>%
+  clean_names() %>%
+  slice(-n()) %>%
+  select(-starts_with("x1")) %>%
+  # normalise col names for weather in some surveys
+  rename_with(~ str_replace(.x, "_c$", ""), .cols = ends_with("_c"))
 
 label_risk <- function(x) {
   pct_risk <- scales::percent(abs(x - 1))
@@ -24,7 +50,9 @@ risk_subtitle <- function(risk, disadvantage, advantage, bad_outcome,
     "{if_else(statsig, statsig_emoji, '')}",
     "People {disadvantage} were ",
     "{label_risk(attr(risk, 'RR'))} ",
-    "to {bad_outcome} than {advantage}.")
+    "to {bad_outcome} than {advantage}.",
+    " (RR: {label_risk(attr(risk, 'lower.RR'))} to {label_risk(attr(risk, 'upper.RR'))})"
+    )
 }
 
 rate_diff_plot <- function(df, predictor, outcome, bad_outcome, good_outcome,
