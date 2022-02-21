71013e63fe868d142f31d84d54a5b8a653be8392
James Goldie <james.goldie@360info.org>
2022-02-21 12:04:57 +1100

    Tidy up survey import

--- util.r
+++ util.r
@@ -4,54 +4,58 @@ library(cli)
 
 # pipe fn to import surveys and do light tidying
 import_survey <- . %>%
-  {
-    read_csv(., na = c("", "NA", "N/A", "N / A"),
-      col_types = cols(
-        "OptIn Date" = col_date(format = "%d/%m/%Y"),
-        HoursDay = col_integer(),
-        Rooms = col_integer(),
-        Windows = col_integer(),
-        "People in House" = col_number(),
-        Temperature = col_number(),
-        "Temperature (C)" = col_number(),
-        "Relative Humidity" = col_number(),
-        "Heat Index" = col_number(),
-        "Heat Index (C)" = col_number(),
-        .default = col_factor(NULL, include_na = FALSE)
-        # .default = col_character()
-    ))
-  } %>%
+  read_csv(na = c("", "NA", "N/A", "N / A"),
+    col_types = cols(
+      "OptIn Date" = col_date(format = "%d/%m/%Y"),
+      HoursDay = col_integer(),
+      Rooms = col_integer(),
+      Windows = col_integer(),
+      "People in House" = col_number(),
+      Temperature = col_number(),
+      "Temperature (C)" = col_number(),
+      "Relative Humidity" = col_number(),
+      "Heat Index" = col_number(),
+      "Heat Index (C)" = col_number(),
+      .default = col_factor(NULL, include_na = FALSE))) %>%
   clean_names() %>%
   slice(-n()) %>%
   select(-starts_with("x1")) %>%
   # normalise col names for weather in some surveys
   rename_with(~ str_replace(.x, "_c$", ""), .cols = ends_with("_c"))
 
+# formatting fns for readable labels
+
 label_risk <- function(x) {
   pct_risk <- scales::percent(abs(x - 1))
   moreless <- ifelse(x > 1, "more", "less")
   paste(pct_risk, moreless, "likely")
 }
+
 label_range <- function(x) {
   paste(
     scales::percent(attr(x, "lower.RR")), "to",
     scales::percent(attr(x, "upper.RR")))
 }
+
 rr_statsig <- function(x) {
   !between(1, attr(x, "lower.RR"), attr(x, "upper.RR"))
 }
 
 risk_subtitle <- function(risk, disadvantage, advantage, bad_outcome,
-  statsig = FALSE) {
+  statsig = FALSE, rr = FALSE) {
 
   statsig_emoji <- if_else(rr_statsig(risk), '☑️', '❌')
+  rr_text <- glue(
+    " (RR: {label_risk(attr(risk, 'lower.RR'))}", " to",
+    " {label_risk(attr(risk, 'upper.RR'))})")
 
   glue(
     "{if_else(statsig, statsig_emoji, '')}",
-    "People {disadvantage} were ",
-    "{label_risk(attr(risk, 'RR'))} ",
-    "to {bad_outcome} than {advantage}.",
-    " (RR: {label_risk(attr(risk, 'lower.RR'))} to {label_risk(attr(risk, 'upper.RR'))})"
+    "Surveyed respondents in Indonesia, Pakistan, India and Cameroon<br>",
+    "{disadvantage} were **{label_risk(attr(risk, 'RR'))}** to {bad_outcome}<br>",
+    "than people {advantage}.",
+    "{if_else(rr, rr_text, '')}"
+    # " (RR: {label_risk(attr(risk, 'lower.RR'))} to {label_risk(attr(risk, 'upper.RR'))})"
     )
 }
 
@@ -83,15 +87,18 @@ rate_diff_plot <- function(df, predictor, outcome, bad_outcome, good_outcome,
 
   # ceate the plot
   risk_plot <- ggplot(filter(df, {{ bad_outcome }})) +
-      aes(y = predictor_prop, x = {{ predictor }}, fill = {{ outcome }}) +
+      aes(y = predictor_prop, x = {{ predictor }}, fill = {{ predictor }}) +
       geom_col() +
       geom_richtext(
+        family = "Body 360info",
         aes(label = glue(
           "**{scales::percent(predictor_prop)} of respondents**<br>",
           "({n} of {predictor_n} surveyed)")),
         colour = "white", fill = NA, label.colour = NA,
-        hjust = "inward", nudge_y = -0.00125, size = 5) +
+        hjust = "inward", nudge_y = -0.01, size = 5.5) +
       scale_y_continuous(labels = scales::label_percent()) +
+      scale_fill_manual(
+        values = c(pal_360[["blue"]], pal_360[["darkblue"]])) +
       coord_flip() +
       labs(subtitle = risk_subtitle(risk, phrase_disadvantage, phrase_advantage,
         phrase_bad_outcome))
